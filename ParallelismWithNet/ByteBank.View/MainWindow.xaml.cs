e6f000dc2dbb60dbb423ff75a3c6d7bc01bfdfe0
Douglas Lima <Douglasproglima@gmail.com>
2022-02-22 23:53:46 -0300

    Clean libraries not used

--- ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
+++ ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
@@ -1,21 +1,8 @@
-﻿using ByteBank.Core.Model;
-using ByteBank.Core.Repository;
+﻿using ByteBank.Core.Repository;
 using ByteBank.Core.Service;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ByteBank.View
 {

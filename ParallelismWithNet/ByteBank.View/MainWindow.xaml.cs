e6e34d147699b9138096aca392834764c99d0da4
Douglas Lima <Douglasproglima@gmail.com>
2022-02-24 22:50:53 -0300

    Refactore method

--- ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
+++ ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using ByteBank.Core.Repository;
+﻿using ByteBank.Core.Model;
+using ByteBank.Core.Repository;
 using ByteBank.Core.Service;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -20,35 +22,38 @@ namespace ByteBank.View
 
             r_Repositorio = new ContaClienteRepository();
             r_Servico = new ContaClienteService();
-            BtnProcessar.IsEnabled = false;
         }
 
         private void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
+            BtnProcessar.IsEnabled = false;
             //Retorna a thread principal que está sendo executado no momento
             var taskScheduleUI = TaskScheduler.FromCurrentSynchronizationContext();
-
             var contas = r_Repositorio.GetContaClientes();
-
-            var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
+
+
             /*Objetivo: Transformar a lista de contas em uma lista de tarefas(Consolidade a conta de um ou mais clientes)*/
+            /*
             var contasTarefas = contas.Select(conta =>
             {
-                /*Task.Factory: Constroi as tarefas
-                 *TaskScheduler: Define quando/onde uma tarefa será executada nos cores
-                 * OBSERVACAO: Quando se usa o Task.Factory, automáticamente estamos usando o "TaskScheduler"
-                 */
+            */
+            /*Task.Factory: Constroi as tarefas
+             *TaskScheduler: Define quando/onde uma tarefa será executada nos cores
+             * OBSERVACAO: Quando se usa o Task.Factory, automáticamente estamos usando o "TaskScheduler"
+             */
+            /*
                 return Task.Factory.StartNew(() =>
                 {
                     var retornoConta = r_Servico.ConsolidarMovimentacao(conta);
                     resultado.Add(retornoConta);
                 });
             }).ToArray();
+            */
 
             //Task.WaitAll: Aguarda a execução de todas as threads
             //Enquanto não executar todas as threads não passa para linha debaixo.
@@ -57,12 +62,16 @@ namespace ByteBank.View
             /*Diferente do WaitAll(), o WhenAll(): Ao invés de executar a thread que executa a função para travar e aguardar as outras
              * tarefas(threads), ele retornar uma outra threads, cujo a função é aguardar a execução das demais threads
              */
-            Task.WhenAll(contasTarefas)
+
+            RetonarConsolidarConta(contas)
                 //Só executa quando a thread anterior terminar
                 .ContinueWith((task) =>
                 {
                     //Aqui, consigo pegar a thread que originou a execução, dessa forma tem info do retorno, dos erros etc...
                     var fim = DateTime.Now;
+
+                    var resultado = task.Result; // Result vem do método RetonarConsolidarConta(conta)
+
                     AtualizarView(resultado, fim - inicio);
                 }, taskScheduleUI /*Será executado de acordo com a demada da task principal*/)
                 .ContinueWith((task) =>
@@ -73,6 +82,22 @@ namespace ByteBank.View
             //Movi esse código para execução da thread .ContinueWith()
             //var fim = DateTime.Now;
             //AtualizarView(resultado, fim - inicio);
+        }
+
+        private Task<List<string>> RetonarConsolidarConta(IEnumerable<ContaCliente> contas)
+        {
+            var resultado = new List<string>();
+
+            var tasks = contas.Select( conta => 
+            {
+                return Task.Factory.StartNew(() =>
+               {
+                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+                   resultado.Add(resultadoConta);
+               });
+            });
+
+            return Task.WhenAll(tasks).ContinueWith( task => resultado);
         }
 
         private void AtualizarView(List<String> result, TimeSpan elapsedTime)

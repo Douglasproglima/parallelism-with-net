758b22eb5f410cc225ed25bb1f0c99afa960c792
Douglas Lima <Douglasproglima@gmail.com>
2022-02-24 23:02:35 -0300

    Used async/await

--- ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
+++ ParallelismWithNet/ByteBank.View/MainWindow.xaml.cs
@@ -24,86 +24,43 @@ namespace ByteBank.View
             r_Servico = new ContaClienteService();
         }
 
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
-            BtnProcessar.IsEnabled = false;
-            //Retorna a thread principal que está sendo executado no momento
-            var taskScheduleUI = TaskScheduler.FromCurrentSynchronizationContext();
+            HabilitarBotaoProcessar(false);
+
             var contas = r_Repositorio.GetContaClientes();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
-
-
-
-            /*Objetivo: Transformar a lista de contas em uma lista de tarefas(Consolidade a conta de um ou mais clientes)*/
-            /*
-            var contasTarefas = contas.Select(conta =>
-            {
-            */
-            /*Task.Factory: Constroi as tarefas
-             *TaskScheduler: Define quando/onde uma tarefa será executada nos cores
-             * OBSERVACAO: Quando se usa o Task.Factory, automáticamente estamos usando o "TaskScheduler"
-             */
-            /*
-                return Task.Factory.StartNew(() =>
-                {
-                    var retornoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(retornoConta);
-                });
-            }).ToArray();
-            */
-
-            //Task.WaitAll: Aguarda a execução de todas as threads
-            //Enquanto não executar todas as threads não passa para linha debaixo.
-            //Task.WaitAll(contasTarefas);
-
-            /*Diferente do WaitAll(), o WhenAll(): Ao invés de executar a thread que executa a função para travar e aguardar as outras
-             * tarefas(threads), ele retornar uma outra threads, cujo a função é aguardar a execução das demais threads
-             */
-
-            RetonarConsolidarConta(contas)
-                //Só executa quando a thread anterior terminar
-                .ContinueWith((task) =>
-                {
-                    //Aqui, consigo pegar a thread que originou a execução, dessa forma tem info do retorno, dos erros etc...
-                    var fim = DateTime.Now;
-
-                    var resultado = task.Result; // Result vem do método RetonarConsolidarConta(conta)
-
-                    AtualizarView(resultado, fim - inicio);
-                }, taskScheduleUI /*Será executado de acordo com a demada da task principal*/)
-                .ContinueWith((task) =>
-                {
-                    BtnProcessar.IsEnabled = true;
-                }, taskScheduleUI);
-
-            //Movi esse código para execução da thread .ContinueWith()
-            //var fim = DateTime.Now;
-            //AtualizarView(resultado, fim - inicio);
+            var resultadoConsolidadeConta = await RetonarConsolidarConta(contas);
+            var fim = DateTime.Now;
+            
+            AtualizarView(resultadoConsolidadeConta, fim - inicio);
+            
+            HabilitarBotaoProcessar(true);
         }
 
-        private Task<List<string>> RetonarConsolidarConta(IEnumerable<ContaCliente> contas)
+        private void HabilitarBotaoProcessar(bool isHabilitar)
         {
-            var resultado = new List<string>();
-
-            var tasks = contas.Select( conta => 
-            {
-                return Task.Factory.StartNew(() =>
-               {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                   resultado.Add(resultadoConta);
-               });
-            });
-
-            return Task.WhenAll(tasks).ContinueWith( task => resultado);
+            BtnProcessar.IsEnabled = isHabilitar;
         }
 
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        private async Task<string[]> RetonarConsolidarConta(IEnumerable<ContaCliente> contas)
+        {
+            var tasks = contas.Select(conta =>
+                Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta))
+            );
+
+            var resultado = await Task.WhenAll(tasks);
+
+            return resultado;
+        }
+
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
